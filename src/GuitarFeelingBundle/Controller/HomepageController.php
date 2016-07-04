bee71b7f9732d1c25ddda246b27090c2c39e7972
mspr <spriet.maxime.eisti@gmail.com>
2016-07-04 10:32:58 +0200

    Remove the default controller and add a Homepage controller

--- src/GuitarFeelingBundle/Controller/HomepageController.php
+++ src/GuitarFeelingBundle/Controller/HomepageController.php
@@ -4,10 +4,10 @@ namespace GuitarFeelingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class HomepageController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('GuitarFeelingBundle:Default:index.html.twig');
+        return $this->render('GuitarFeelingBundle:Homepage:index.html.twig');
     }
 }
