1a4caf05488181d21f0852f42779741b1088dc5e
mspr <spriet.maxime.eisti@gmail.com>
2016-07-19 15:43:34 +0200

    Implement show action for tutorials to display tutorial information after creation

--- src/GuitarFeelingBundle/Controller/TutorialController.php
+++ src/GuitarFeelingBundle/Controller/TutorialController.php
@@ -9,37 +9,42 @@ use Symfony\Component\HttpFoundation\Request;
 
 class TutorialController extends Controller
 {
-      public function indexAction()
-      {
-         $em = $this->getDoctrine()->getEntityManager();
-         $tutorials = $em->getRepository('GuitarFeelingBundle:Tutorial')->findAll();
-         
-         return $this->render('GuitarFeelingBundle:Tutorial:index.html.twig', array('tutorials' => $tutorials));
-      }
+   public function indexAction()
+   {
+      $em = $this->getDoctrine()->getEntityManager();
+      $tutorials = $em->getRepository('GuitarFeelingBundle:Tutorial')->findAll();
+      
+      return $this->render('GuitarFeelingBundle:Tutorial:index.html.twig', array('tutorials' => $tutorials));
+   }
 
-      public function newAction(Request $request)
+   public function newAction(Request $request)
+   {
+      $tutorial = new Tutorial();
+      
+      $form = $this->createForm(TutorialType::class, $tutorial);
+      
+      $form->handleRequest($request);
+      if ($form->isValid())
       {
-         $tutorial = new Tutorial();
+         $em = $this->getDoctrine()->getManager();
+         $em->persist($tutorial);
+         $em->flush();
          
-         $form = $this->createForm(TutorialType::class, $tutorial);
-         
-         $form->handleRequest($request);
-         if ($form->isValid())
-         {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($tutorial);
-            $em->flush();
-            
-            $request->getSession()->getFlashBag()->add('notice', 'Tutorial created.');
-         
-            return $this->redirect($this->generateUrl('guitar_feeling_tutorials_show', array('id' => $tutorial->getId())));
-         }
-         
-         return $this->render('GuitarFeelingBundle:Tutorial:new.html.twig', array('form' => $form->createView()));
+         $request->getSession()->getFlashBag()->add('notice', 'Tutorial created.');
+      
+         return $this->redirect($this->generateUrl('guitar_feeling_tutorials_show', array('id' => $tutorial->getId())));
       }
+      
+      return $this->render('GuitarFeelingBundle:Tutorial:new.html.twig', array('form' => $form->createView()));
+   }
 
-      public function showAction()
-      {
-         return $this->render('GuitarFeelingBundle:Tutorial:show.html.twig');
-      }
+   public function showAction($id)
+   {
+      $em = $this->getDoctrine()->getEntityManager();
+      $tutorial = $em->getRepository('GuitarFeelingBundle:Tutorial')->find($id);
+      if (!$tutorial)
+         $this->createNotFoundException('Unable to find Tutorial entity.');
+      
+      return $this->render('GuitarFeelingBundle:Tutorial:show.html.twig', array('tutorial' => $tutorial));
+   }
 }
