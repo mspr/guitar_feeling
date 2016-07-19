f1d45baa5efe3b0150ab800a20bcbbe7eea07906
mspr <spriet.maxime.eisti@gmail.com>
2016-07-19 10:47:45 +0200

    Add newAction with a form and showAction with related views

--- src/GuitarFeelingBundle/Controller/TutorialController.php
+++ src/GuitarFeelingBundle/Controller/TutorialController.php
@@ -3,6 +3,9 @@
 namespace GuitarFeelingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use GuitarFeelingBundle\Entity\Tutorial;
+use GuitarFeelingBundle\Form\TutorialType;
+use Symfony\Component\HttpFoundation\Request;
 
 class TutorialController extends Controller
 {
@@ -11,8 +14,29 @@ class TutorialController extends Controller
          return $this->render('GuitarFeelingBundle:Tutorial:index.html.twig');
       }
 
-      public function newAction()
+      public function newAction(Request $request)
       {
-         return $this->render('GuitarFeelingBundle:Tutorial:new.html.twig');
+         $tutorial = new Tutorial();
+         
+         $form = $this->createForm(TutorialType::class, $tutorial);
+         
+         $form->handleRequest($request);
+         if ($form->isValid())
+         {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($tutorial);
+            $em->flush();
+            
+            $request->getSession()->getFlashBag()->add('notice', 'Tutorial created.');
+         
+            return $this->redirect($this->generateUrl('guitar_feeling_tutorials_show', array('id' => $tutorial->getId())));
+         }
+         
+         return $this->render('GuitarFeelingBundle:Tutorial:new.html.twig', array('form' => $form->createView()));
+      }
+
+      public function showAction()
+      {
+         return $this->render('GuitarFeelingBundle:Tutorial:show.html.twig');
       }
 }
---
+++ src/GuitarFeelingBundle/Form/TutorialType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace GuitarFeelingBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class TutorialType extends AbstractType
+{
+   public function buildForm(FormBuilderInterface $builder, array $options)
+   {
+      $builder->add('title');
+      $builder->add('description');
+      $builder->add('category', EntityType::class, array(
+         'class' => 'GuitarFeelingBundle:TutorialCategory',
+         'choice_label' => 'name'
+      ));
+      $builder->add('level', EntityType::class, array(
+         'class' => 'GuitarFeelingBundle:TutorialLevel',
+         'choice_label' => 'name'
+      ));
+      $builder->add('create', SubmitType::class);
+   }
+
+   public function getName()
+   {
+      return 'tutorial';
+   }
+}
