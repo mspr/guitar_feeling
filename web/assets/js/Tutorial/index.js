355af68258019fbc4bb6c0522ca87f0af97f0d92
mspr <spriet.maxime.eisti@gmail.com>
2016-10-05 09:52:17 +0200

    Remove useless variable in Tutorial index jquery method.

--- web/assets/js/Tutorial/index.js
+++ web/assets/js/Tutorial/index.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
    
    $( ".star-rating" ).each(function(index) {
-      var name = $(this).attr("name");
       var level = $(this).attr("value");
       var starRatingRdbs = $(this).find('.star-rating__input');
       var starRaintsRdbCount = starRatingRdbs.length;
