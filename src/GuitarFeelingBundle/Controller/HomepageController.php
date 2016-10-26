689ca3ff3c400c5e824245e56ddb5a348ec64acb
mspr <spriet.maxime.eisti@gmail.com>
2016-10-26 16:20:45 +0200

    Add carousel to show the most recent created tutorials from the home page.

--- src/GuitarFeelingBundle/Controller/HomepageController.php
+++ src/GuitarFeelingBundle/Controller/HomepageController.php
@@ -6,8 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomepageController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('GuitarFeelingBundle:Homepage:index.html.twig');
-    }
+   public function indexAction()
+   {
+      $em = $this->getDoctrine()->getEntityManager();
+      
+      $mostRecentTutorials = $this->getMostRecentTutorials(3);
+      $levels = $em->getRepository('GuitarFeelingBundle:TutorialLevel')->findAll();
+      
+      return $this->render('GuitarFeelingBundle:Homepage:index.html.twig', array('mostRecentTutorials' => $mostRecentTutorials, 'levels' => $levels));
+   }
+    
+   private function getMostRecentTutorials($tutorialCount)
+   {
+      $em = $this->getDoctrine()->getEntityManager();
+      $repository = $em->getRepository('GuitarFeelingBundle:Tutorial');
+      
+      $query = $repository->createQueryBuilder('t')
+         ->orderBy('t.createdAt', 'DESC')
+         ->setMaxResults($tutorialCount)
+         ->getQuery();
+      
+      return $query->getResult();
+   }
 }
---
+++ web/assets/js/Homepage/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+   
+   $( ".star-rating" ).each(function(index) {
+      var level = $(this).attr("value");
+      var starRatingRdbs = $(this).find('.star-rating__input');
+      var starRaintsRdbCount = starRatingRdbs.length;
+      var element = $(this).find('.star-rating__input')[starRaintsRdbCount - level];
+      $(element).attr("checked", true);
+   });
+   
+   $('.star-rating__ico').attr("disabled", true);
+   $('.star-rating__input').attr("disabled", true);
+});
